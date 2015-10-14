55369716cd8bbfcb7c0e9c135b07f04b63ae2eba
Tim Boden <mail@timboden.de>
2015-10-14 18:38:09 +0200

    test

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var func = function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hallo Welt!');
-}
+};
 var server = http.createServer(func).listen(8080);
 
 console.log('Der Webserver ist erreichbar unter http://*:8080/');
