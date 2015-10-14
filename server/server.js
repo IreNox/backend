f049046d03b02bb59dd6ece73e348f55cafff7c6
Tim Boden <mail@timboden.de>
2015-10-14 19:04:56 +0200

    ...

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var func = function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hallo Welt!');
+	res.end('Hallo Welt 2!');
 };
 var server = http.createServer(func).listen(8080);
 
