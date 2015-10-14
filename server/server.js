ef7b0262d527d6a312bea7318b204fd9df2eb271
Tim Boden <mail@timboden.de>
2015-10-14 15:26:37 +0200

    add more test

---
+++ server/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var func = function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hallo Welt!');
+}
+var server = http.createServer(func).listen(8080);
+
+console.log('Der Webserver ist erreichbar unter http://*:8080/');
--- update_script/update.sh
+++ update_script/update.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+cd ..
 
+cd server
+forever stop server.js
+cd ..
+
+git pull
+
+cd server
+forever start server.js
+cd ..
+
+cd update_script
