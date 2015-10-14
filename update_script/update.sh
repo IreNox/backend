72bf51fe5d111c627d1bdcc9284b5f95d86a9777
Tim Boden <mail@timboden.de>
2015-10-14 18:57:54 +0200

    test script

--- update_script/update.sh
+++ update_script/update.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 cd ..
 
-cd server
-forever stop server.js
-cd ..
+# check for git updates
+check_for_update() {
+	git fetch origin
+	git_diff=`git diff origin/master`
+	if [ -n "$git_diff" ]
+		then return 1
+	fi
+	
+	return 0
+}
 
-git pull
+function update_data() {
+	cd server
+	forever stop server.js
+	cd ..
 
-cd server
-forever start server.js
-cd ..
+	git merge origin/master
+
+	cd server
+	forever start server.js
+	cd ..
+}
+
+if [ check_for_update ]
+	then update_data
+fi
 
 cd update_script
